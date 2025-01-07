befba9c7c00bdfcad9978fe65a0dde6c15cca9b6
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-07 18:46:11 +1100

    created repo and files

---
+++ binary_trees.h
@@ -0,0 +1,13 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+
+/* Task function prototypes*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+#endif // !BINARY_TREES_H
+
