1c00e7e4307c7588684751b4ce0aeded74ca41dd
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-07 19:08:43 +1100

    added struct to header file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,17 +11,19 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new;
+  binary_tree_t *new_node;
 
-  new = malloc(sizeof(binary_tree_t));
-  if (new == NULL) 
-        return NULL;
+  /* This allocates memory for the new node*/
+  new_node = malloc(sizeof(binary_tree_t));
+  if (new_node) 
+        return (NULL); /* This will return if malloc FAILS */
 
-  new->n = value;
-  new->parent = parent; 
-  new->left = NULL;
-  new->right = NULL;
+  /*Node initiliaztion process*/
+  new_node->n = value; 
+  new_node->parent = parent; 
+  new_node->left = NULL; /* No left child */
+  new_node->right = NULL; /* No RIght child */
 
-  return (new);
+  return (new_node);
 }
 
--- binary_trees.h
+++ binary_trees.h
@@ -1,13 +1,31 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
 
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * struct binary_tree_s - Binary tree node
+ * 
+ * @n: Integer tored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+struct binary_tree_struct 
+{
+    int n; 
+    struct binary_tree_struct *parent;
+    struct binary_tree_struct *left;
+    struct binary_tree_struct *right;
+    
+};
+typedef struct binary_tree_struct binary_tree_t;
 
 /* Task function prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
-#endif // !BINARY_TREES_H
+#endif // !BINARY_TREE_H
 
