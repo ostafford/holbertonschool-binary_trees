bc3dac8c910fe41d8b77d33e87efdd620cab7acb
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-10 22:16:49 +1100

    created required file 2-binary

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,6 @@ typedef struct binary_tree_struct binary_tree_t;
 /* Task function prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value); 
-
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent int value); 
 
 #endif
