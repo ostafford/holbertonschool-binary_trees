f8eb2365d0578d77184f5923a54539084dedb45d
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-11 21:57:28 +1100

    udpated betty style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,9 +10,9 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  return (is_full_recursive(tree));
+if (tree == NULL)
+return (0);
+return (is_full_recursive(tree));
 }
 
 /**
@@ -25,15 +25,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 int is_full_recursive(const binary_tree_t *tree)
 {
-  if (tree != NULL)
-  {
-    if ((tree->left != NULL && tree->right == NULL) ||
-        (tree->left == NULL && tree->right != NULL) ||
-        is_full_recursive(tree->left) == 0 ||
-        is_full_recursive(tree->right) == 0 )
+if (tree != NULL)
+{
+if ((tree->left != NULL && tree->right == NULL) ||
+(tree->left == NULL && tree->right != NULL) ||
+is_full_recursive(tree->left) == 0 ||
+is_full_recursive(tree->right) == 0 )
 
-      return (0);
-  }
-  return (1);
+return (0);
+}
+return (1);
 }
 
