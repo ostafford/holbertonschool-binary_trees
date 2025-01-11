9b8c92ed29031b5c8caf02bdcd265e908f727307
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-11 15:45:51 +1100

    Udpated betty style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,7 +26,7 @@ return (0);
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (tree) 
+if (tree)
 {
 size_t left = 0, right = 0;
 
@@ -34,6 +34,6 @@ left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 return ((left > right) ? left : right);
 }
-return (0); 
+return (0);
 }
 
