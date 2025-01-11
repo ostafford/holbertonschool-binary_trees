b6d6cffb8a3c5de9cb50a71ed046fd1c771124d7
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-11 12:21:14 +1100

    udpated compiler errors.

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,10 +12,9 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (tree && func)
 {
-func(tree->node);
+func(tree->n);
 binary_tree_preorder(tree->left, func);
 binary_tree_preorder(tree->right, func);
 }
-return (0);
 }
 
