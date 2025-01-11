60c86e5d3c26ae3f7777668151a6166b612e3a61
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-11 14:57:56 +1100

    updated betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,11 +11,10 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 if (tree && tree->parent)
+return (1 + binary_tree_depth(tree->parent));
+else
 {
-    return 1 + binary_tree_depth(tree->parent);
+return (0);
 }
-  else {
-    return (0);
-  }
 }
 
