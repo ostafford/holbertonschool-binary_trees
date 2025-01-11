892014f7dbc5fb473b7b45a5a2ddbe1d9c4808fa
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-11 15:43:25 +1100

    Updated code for checker errors

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,14 +10,10 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-  {
-    return (0);
-  }
-  int left_height = binary_tree_height(tree->left);
-  int right_height = binary_tree_height(tree->right);
+  if (tree)
+    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-  return (left_height - right_height);
+  return (0);
 }
 
 /**
@@ -35,8 +31,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
     size_t left = 0, right = 0;
 
     left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-    right = tree-> right ? 1 + binary_tree_height(tree->right) : 1;
-
+    right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
     return ((left > right) ? left : right);
   }
   return (0); 
