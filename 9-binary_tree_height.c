4b9072e869329f29f0281ae4b3603e5ef83fba1f
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-11 12:53:43 +1100

    corrected parentheses in correct order and added parentheses in return

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,10 +14,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
     size_t left = 0, right = 0;
 
-    left = tree->left ? 1 + binary_tree_height)tree->left) : 0;
+    left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
     right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    return (left > right ? left : right);
+    return ((left > right) ? left : right);
   }
   return (0);
 }
