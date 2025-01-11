3373ff21849291762bfe7a570c1b9d6cd4c48d99
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-11 12:54:38 +1100

    updated betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,15 +10,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  if (tree) 
+if (tree) 
 {
-    size_t left = 0, right = 0;
+size_t left = 0, right = 0;
 
-    left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    return ((left > right) ? left : right);
-  }
-  return (0);
+return ((left > right) ? left : right);
+}
+return (0);
 }
 
